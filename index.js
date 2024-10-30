8c0aaf9a9a923c8b5cd84c3f4cc7dd09559e73b5
Elliott <elliottlwh@gmail.com>
2024-10-30 09:48:28 +0800

    working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 let path = require('path');
 const cors = require('cors');
 const {Pool} = require('pg');
+const { error } = require("console");
 require("dotenv").config()
 
 const app = express();
@@ -32,8 +33,8 @@ getPostgresVersion()
 app.post("/signUp", async (req,res) => {
   const client = await pool.connect()
   try {
-    const query = "INSERT INTO userinfo (firebaseuid, name, useraddress) VALUES ($1, $2, $3) RETURN *"
-    const params = [req.body.firebaseuid, req.body.name, req.body.useraddress]
+    const query = "INSERT INTO userinfo (firebaseuid, name, useraddress, age, gender) VALUES ($1, $2, $3, $4, $5) RETURNING *"
+    const params = [req.body.firebaseuid, req.body.name, req.body.useraddress, req.body.age, req.body.gender]
     const result = await client.query(query, params)
     res.json(result.rows[0])
   } catch (err) {
@@ -44,8 +45,87 @@ app.post("/signUp", async (req,res) => {
   }
 })
 
+app.get("/profile/:uid", async(req,res) => {
+  const client = await pool.connect()
+  try {
+    const query = "SELECT * FROM userinfo WHERE firebaseuid = $1"
+    const params = [req.params.uid]
+    const result = await client.query(query, params)
+    res.status(200).json(result.rows[0])
+  } catch (err) {
+    console.error(err.stack)
+    res.status(500).json({ error: err.message})
+  } finally {
+    client.release()
+  }
+})
+
+app.post("/meeting", async(req,res) => {
+  const client = await pool.connect()
+  try {
+
+    const userCheck = await client.query("SELECT id FROM userinfo WHERE firebaseuid = $1",[req.body.firebaseuid])
+    const user_id = userCheck.rows[0].id
+
+    let location_id = null
+    const locationCheck = await client.query("SELECT id FROM locations WHERE address = $1",[req.body.address])
+
+    if (locationCheck.rows.length > 0) {
+      location_id = locationCheck.rows[0].id
+    } else {
+      const locationcreate = await client.query("INSERT INTO locations (address) VALUES ($1) RETURNING id",[req.body.address])
+      location_id = locationcreate.rows[0].id
+    }
+
+    
+    const query = "INSERT INTO meetings (date, time, location_id, title) VALUES ($1, $2, $3, $4) RETURNING *"
+    const params = [req.body.date, req.body.time, location_id, req.body.title]
+    const result = await client.query(query, params)
+
+    const meeting_id = result.rows[0].id
+    const userConnectQuery = "INSERT INTO userinfomeetings (user_id, meeting_id) VALUES ($1, $2)"
+    const userConnectParams = [user_id, meeting_id]
+    const userConnectResult = await client.query(userConnectQuery,userConnectParams)
+
+    res.json(result.rows[0])
+    
+  } catch (err) {
+    console.error(err.stack)
+    res.status(500).json({ error: err.message })
+  } finally {
+    client.release()
+  }
+})
+
+app.get("/meetings", async(req,res) => {
+  const client = await pool.connect()
+  try {
+    const query = "SELECT meetings.*, userinfo.name, locations.address FROM userinfomeetings INNER JOIN meetings ON userinfomeetings.meeting_id = meetings.id INNER JOIN userinfo ON userinfomeetings.user_id = userinfo.id INNER JOIN locations ON meetings.location_id = locations.id"   
+    const result = await client.query(query)
+    res.status(200).json(result.rows)
+  } catch (err) {
+    console.error(err.stack)
+    res.status(500).json( {error: err.message })
+  } finally {
+    client.release()
+  }
+})
+
+app.get("/address/:id", async(req,res) => {
+  const client = await pool.connect()
+  try {
+    const result = await client.query("SELECT * FROM locations WHERE id = $1",[req.params.id])
+    res.status(200).json(result.rows[0])
+  } catch (err) {
+    console.error(err.stack)
+    res.status(500).json({ error: err.message})
+  } finally {
+    client.release()
+  }
+})
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(3002, () => console.log("Server ready on port."));
 
 module.exports = app;
